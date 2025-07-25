65e61c841f875af10f6a2df633d972b358c3e5be
NizamUddin07 <nizzamuddin71@gmail.com>
2025-07-25 22:38:28 +0600

    side bar fixed with mediaquery

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,77 +6,101 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
-          Row(
-            children: [
-              Expanded(child:
-              Container(
-                color: secondaryColor,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8, top: 32),
-                      child: Image.asset('images/more icon.png', width: 500,),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: Text('Green', style: TextStyle(fontSize: 15),),
-                    ),
-                    SizedBox(height: 30),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 14),
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                        child: RichText(
-                            text: TextSpan(
-                          children:[
-                            TextSpan(
-                                text: ".", style: TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold,
-                                color: primaryColor
-                            ),
-                            ),
-                            TextSpan(
-                                text: "Shape Class", style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: primaryColor
-                            ),
-                            ),
-                          ]
-                        ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RotatedBox(
-                        quarterTurns: 3,
-                        child: Text("Indor Plants",style: TextStyle(color: Colors.black),),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RotatedBox(
-                        quarterTurns: 3,
-                        child: Text("Indor Plants",style: TextStyle(color: Colors.black),),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-              )
-            ],
+          Row(children: [sideBar(),
+          Container(
+            width: size.width*0.8,
           )
+          ]),
         ],
+      ),
+    );
+  }
+
+  Expanded sideBar() {
+    return Expanded(
+      child: Container(
+        color: secondaryColor,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 30),
+              child: Image.asset('images/more icon.png', width: 60),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text('Green', style: TextStyle(fontSize: 15)),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.only(right: 14),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: ".",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: primaryColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "Shape Class",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Text(
+                  "Indor Plants",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Text(
+                  "Indor Plants",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 110, right: 16),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  color: primaryColor,
+                ),
+                width: 50,
+                height: 60,
+                child: Icon(Icons.home, color: Colors.white, size: 40),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
